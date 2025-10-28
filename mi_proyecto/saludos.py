ba95fe83b032ce3e6c0260ba205b59ff69a2878c
Marcos-ArPo <marcos.arbin.pozo.al@iespoligonosur.org>
2025-10-28 17:20:50 +0100

    feat: Añade saludo formal en español

--- mi_proyecto/saludos.py
+++ mi_proyecto/saludos.py
@@ -1,7 +1,7 @@
 from utils.printer import print_message
 def saludo(name: str):
     """Genera un saludo simple."""
-    message = f"Hola, {name}!"
+    message = f"BUenos dias, {name}. Espero que tengas un excelente dia."
     print_message(message)
     
 def greeting(name: str):
